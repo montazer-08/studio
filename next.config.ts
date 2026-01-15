74477e16d481d7dd17b64e423871fa62b92c2648
montazer-09 <montazer09@gmail.com>
2026-01-15 16:41:53 +0300

    fix: disable static export for server actions

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,27 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
+  reactStrictMode: true,
+
   images: {
-    unoptimized: true, // Required for static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
       },
     ],
   },
 };
 
-export default nextConfig;
+export default nextConfig;	
