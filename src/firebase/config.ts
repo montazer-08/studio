49f3e489303c2e75ca3941179880b343e4ee4666
montazer-08 <ghkv04885@gmail.com>
2026-01-15 20:36:02 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,14 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+
 export const firebaseConfig = {
-  "projectId": "studio-8785153620-4e332",
-  "appId": "1:371700283438:web:2f271b1011c93b8a8747bf",
-  "apiKey": "AIzaSyDdNbLstyhRz5bykdh3zRk7OenK63md8BA",
-  "authDomain": "studio-8785153620-4e332.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "371700283438"
+  apiKey: "AIzaSyDNbLStyrhRZ5bykdh3zRk70emK63mdB8A",
+  authDomain: "studio-8785153620-4e332.firebaseapp.com",
+  projectId: "studio-8785153620-4e332",
+  storageBucket: "studio-8785153620-4e332.appspot.com",
+  messagingSenderId: "371700283438",
+  appId: "1:371700283438:web:2f2471b1011c93b8a874bf",
+  measurementId: "G-XXXXXXXXXX"
 };
+
+export const firebaseApp =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
