d271f5ba04d7be105b848e12862a67313d55a930
montazer-08 <ghkv04885@gmail.com>
2026-01-16 16:01:47 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,12 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDNbLStyrhRZ5bykdh3zRk70emK63mdB8A",
+  apiKey: "AIzaSyDdNbLstyhRz5bykdh3zRk7OenK63md8BA",
   authDomain: "studio-8785153620-4e332.firebaseapp.com",
   projectId: "studio-8785153620-4e332",
-  storageBucket: "studio-8785153620-4e332.appspot.com",
+  storageBucket: "studio-8785153620-4e332.firebasestorage.app",
   messagingSenderId: "371700283438",
-  appId: "1:371700283438:web:2f2471b1011c93b8a874bf",
-  measurementId: "371700283438"
+  appId: "1:371700283438:web:14ed132fe53eecf78747bf"
 };
 
 export const firebaseApp =
